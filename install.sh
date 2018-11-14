b877c6fcc849618af8a23f10ba9558b1d5d02265
Thomas Lane <thomas_lane@icloud.com>
2018-11-13 17:57:01 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ dialog --title "TARBS Post Installation" --infobox "Loading Menu" 5 70
 cd ~/
 git clone https://github.com/allbombson/TARBS-postinstall &>/dev/null
 cd TARBS-postinstall
+tput clear
 sudo bash wizard.sh
 cd ~/
 
